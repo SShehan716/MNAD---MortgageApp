902f7bece3e79feed394b876a08c44f69e98cc37
SShehan716 <sachinshehan20@gmail.com>
2024-11-26 18:49:33 +0530

    Initial Commit

---
+++ MortgageLabApp/MortgageLabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortgageLabAppApp.swift
+//  MortgageLabApp
+//
+//  Created by Sachin shehan on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct MortgageLabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
